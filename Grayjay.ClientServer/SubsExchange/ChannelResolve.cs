905a4fd0fdb36e804bc6cc148b5c54284cb0edf4
Kelvin K <kelvin@futo.org>
2025-03-26 23:30:01 +0100

    WIP Subs exchange

---
+++ Grayjay.ClientServer/SubsExchange/ChannelResolve.cs
@@ -0,0 +1,12 @@
+﻿using Grayjay.Engine.Models.Channel;
+using Grayjay.Engine.Models.Feed;
+
+namespace Grayjay.ClientServer.SubsExchange
+{
+    public class ChannelResolve
+    {
+        public string ChannelUrl { get; set; }
+        public PlatformChannel Channel { get; set; }
+        public PlatformContent[] Content { get; set; }
+    }
+}
---
+++ Grayjay.ClientServer/SubsExchange/ChannelResult.cs
@@ -0,0 +1,13 @@
+﻿using Grayjay.Engine.Models.Channel;
+using Grayjay.Engine.Models.Feed;
+
+namespace Grayjay.ClientServer.SubsExchange
+{
+    public class ChannelResult
+    {
+        public DateTime DateTime { get; set; }
+        public string ChannelUrl { get; set; }
+        public PlatformChannel Channel { get; set; }
+        public PlatformContent[] Content { get; set; }
+    }
+}
