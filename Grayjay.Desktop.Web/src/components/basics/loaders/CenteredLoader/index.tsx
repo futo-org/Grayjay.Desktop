7a4db03b3c8b2b7d4789949f637904b8c06c429d
Koen J <git@unkto.com>
2025-05-01 12:40:43 +0200

    Made a fix for loader not being centered.

--- Grayjay.Desktop.Web/src/components/basics/loaders/CenteredLoader/index.tsx
+++ Grayjay.Desktop.Web/src/components/basics/loaders/CenteredLoader/index.tsx
@@ -1,14 +1,23 @@
-import { Component, Show } from "solid-js";
+import { Component, JSX, Show } from "solid-js";
 import Loader from "../Loader";
 
 export interface LoaderProps {
   size?: string;
   text?: string;
+  style?: JSX.CSSProperties;
 }
 
 const CenteredLoader: Component<LoaderProps> = (props) => {
     return (
-        <div style="width: 100%; height: 100%; flex-direction: column; display: flex; align-items: center; justify-content: center;">
+        <div style={{
+            "width": "100%",
+            "height": "100%",
+            "flex-direction": "column",
+            "display": "flex",
+            "align-items": "center",
+            "justify-content": "center",
+            ... props.style
+        }}>
             <Loader size={props.size} />
             <Show when={props.text && props.text.length}>
                 <div style="margin-top: 20px;">
