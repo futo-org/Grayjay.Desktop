8f8145334bad4c89dc9a38d94a753ec48c180617
Koen J <koen@disper.io>
2025-09-15 16:22:05 +0200

    Reduced itemWidth to 300 for more wide steam deck 3 columns.

--- Grayjay.Desktop.Web/src/components/containers/ContentGrid/index.tsx
+++ Grayjay.Desktop.Web/src/components/containers/ContentGrid/index.tsx
@@ -233,7 +233,7 @@ const ContentGrid: Component<ContentGridProps> = (props) => {
                     addedItems={props.pager?.addedFilteredItemsEvent}
                     modifiedItems={props.pager?.modifiedFilteredItemsEvent}
                     removedItems={props.pager?.removedFilteredItemsEvent}
-                    itemWidth={305}
+                    itemWidth={300}
                     overscan={2}
                     calculateHeight={(width) => {
                         const aspectRatio = 16 / 9;
